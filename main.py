31107a87525c8a52cb2a248799409a060624076d
aftabalisahar <aftabalisahar@gmail.com>
2025-09-17 22:01:42 +0500

    updated

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ async def websocket_extract(websocket: WebSocket):
                 ydl_opts = {
                     "quiet": True,
                     "skip_download": True,
-                    "cookies": "cookies/cookies.txt",
+                    "cookies": "/home/ubuntu/downloader/cookies/youtube_cookies.txt",
                     "format": "bestvideo+bestaudio/best",
                     "cookiefile": COOKIES_FILE  # Cookies for logged-in YouTube
                 }
